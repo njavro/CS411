49d884b3520bf8758b9c5ebaf3681ce8d94f1a23
njavro <njavro@bu.edu>
2020-03-27 12:54:39 -0400

    Added possibility to add and delete tickers to the portfolio

--- Project/demo/finNews/quotes/urls.py
+++ Project/demo/finNews/quotes/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('about.html', views.about, name="about"),
+    path('add_stock.html', views.add_stock, name="add_stock"),
+    path('delete/<stock_id>',views.delete, name="delete"),
 ]
--- Project/demo/finNews/quotes/views.py
+++ Project/demo/finNews/quotes/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Stock
+from .forms import StockForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -22,3 +25,28 @@ def about(request):
 	else:
 		return render(request,'about.html',{'ticker': "Enter a ticket symbol above..."})
 	#API KEY: pk_67513180ad73424ca6137332a00e6ef8
+
+
+def add_stock(request):
+	if request.method == 'POST':
+		form = StockForm(request.POST or None)
+		if form.is_valid():
+			form.save()
+			messages.success(request, ("Stock has been added"))
+			return redirect('add_stock')
+	else:
+		pass
+	ticker = Stock.objects.all()
+
+	return render(request,'add_stock.html',{'ticker':ticker})
+
+
+
+
+
+def delete(request, stock_id):
+	item = Stock.objects.get(pk=stock_id)
+	item.delete()
+
+	messages.success(request,("Stock has been deleted!"))
+	return redirect(add_stock)
