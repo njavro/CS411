f8fb5ea5b11c5058c916bb804741cd9f6073b2ca
njavro <njavro@bu.edu>
2020-03-27 02:07:44 -0400

    Added API loading to website

--- Project/demo/finNews/quotes/views.py
+++ Project/demo/finNews/quotes/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+	
 	return render(request,'index.html',{})
 
 
 def about(request):
-	return render(request,'about.html',{})
+	import requests
+	import json
+	#API KEY: pk_67513180ad73424ca6137332a00e6ef8
+	api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_67513180ad73424ca6137332a00e6ef8")
+
+	try:
+		api = json.loads(api_request.content)
+	except Exception as e:
+		api = "Error..."
+
+
+	return render(request,'about.html',{'api': api})
