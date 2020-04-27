2cce18cc1786b86c91aa4c51c34a331e24fa707d
njavro <njavro@bu.edu>
2020-04-27 18:26:26 -0400

    Added news page

--- Project/demo/finNews/quotes/views.py
+++ Project/demo/finNews/quotes/views.py
@@ -18,9 +18,16 @@ def main(request):
 		api_request = requests.get("https://cloud.iexapis.com/stable/stock/{}/quote?token=pk_67513180ad73424ca6137332a00e6ef8".format(ticker))
 		try:
 			api = json.loads(api_request.content)
+			list_of_stocks = Stock.objects.all() #list Of stocks
 		except Exception as e:
 			api = "Error..."
-		return render(request,'main.html',{'api': api})
+		return render(request,'main.html',{'api': api, 'list_of_stocks': list_of_stocks})
+
+
+		#Google news API
+
+
+
 
 	else:
 		return render(request,'main.html',{'ticker': "Enter a ticket symbol above..."})
@@ -50,6 +57,32 @@ def add_stock(request):
 
 		return render(request,'add_stock.html',{'ticker':ticker, 'output':output})
 
+
+
+
+def stock_news(request):
+	import requests
+	import json
+	from newsapi import NewsApiClient
+
+	if request.method == 'POST':
+		form = StockForm(request.POST or None)
+		if form.is_valid():
+			form.save()
+			messages.success(request, ("Stock has been added"))
+			return redirect('stock_news')
+	else:
+		ticker = Stock.objects.all()
+		output = []
+		for ticker_item in ticker:
+			api_request = requests.get("https://cloud.iexapis.com/stable/stock/{}/quote?token=pk_67513180ad73424ca6137332a00e6ef8".format(str(ticker_item)))
+			try:
+				api = json.loads(api_request.content)
+				output.append(api)
+			except Exception as e:
+				api = "Error..."
+
+		return render(request,'stock_news.html',{'ticker':ticker, 'output':output})
 
 
 
